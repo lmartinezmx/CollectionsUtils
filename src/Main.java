0a6315fd558461b08b3af3e2af41609d9abdcf39
Leonardo <leonardo.lmv@gmail.com>
2019-02-12 16:04:04 -0600

    next, previous and full iterate examples

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,53 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Utilidades para colecciones");
-        iterateAList();
+        //iterateAListNext();
+        //iterateAListPrevious();
+        fullInterate();
 
     }
 
-    private static void iterateAList(){
+    private static void fullInterate() {
+
+        List<String> myStringList;
+        myStringList = Arrays.asList("one","two","REPLACE_ME","three");
+        List dynamicList = new ArrayList(myStringList);
+
+        System.out.println("First list: ");
+        dynamicList.forEach(System.out::println);
+        ListIterator<String> listIterator = dynamicList.listIterator();
+        while(listIterator.hasNext()) {
+            String nextWithIndex = (String) dynamicList.get(listIterator.nextIndex());
+            System.out.println("nextWithIndex: "+nextWithIndex);
+            String next = listIterator.next();
+            System.out.println("next: "+next);
+            if("REPLACE_ME".equals(next)) {
+                listIterator.set("REPLACED");
+            }
+        }
+        System.out.println("After replace: ");
+        dynamicList.forEach(System.out::println);
+        listIterator.add("NEW");
+        System.out.println("After add: ");
+        dynamicList.forEach(System.out::println);
+        while(listIterator.hasPrevious()) {
+            String previousWithIndex
+                    = (String) dynamicList.get(listIterator.previousIndex());
+            System.out.println("previousWithIndex: "+previousWithIndex);
+            String previous = listIterator.previous();
+            System.out.println("previous: "+previous);
+        }
+        System.out.println("After all: ");
+        dynamicList.forEach(System.out::println);
+    }
+
+    private static void iterateAListNext(){
 
         List<String> myStringList;
         myStringList = Arrays.asList("one","two","three");
         List dynamicList = new ArrayList(myStringList);
 
         Iterator<String> myItarator = dynamicList.iterator();
-        ListIterator<String> myListIterator = dynamicList.listIterator(dynamicList.size());
 
         //printing the list
         dynamicList.forEach(System.out::println);
@@ -32,7 +67,24 @@ public class Main {
             System.out.println("afeter remove element: ");
             dynamicList.forEach(System.out::println);
         }
-        //ooor
-        //myItarator.forEachRemaining(System.out::println);
+    }
+
+    private static void iterateAListPrevious(){
+
+        List<String> myStringList;
+        myStringList = Arrays.asList("one","two","three");
+        List dynamicList = new ArrayList(myStringList);
+
+        ListIterator<String> myListIterator = dynamicList.listIterator(dynamicList.size());
+
+        while(myListIterator.hasPrevious()){
+            String previous = myListIterator.previous();
+            //infinite loop :D
+            //myListIterator.add("another_"+myListIterator.previousIndex());
+            System.out.println("previous element: "+previous);
+            dynamicList.forEach(System.out::println);
+        }
+
+
     }
 }
